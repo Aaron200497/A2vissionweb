396a740079e4f17f386fe345549e8effc5e26a45
Aaron200497 <98113453+Aaron200497@users.noreply.github.com>
2025-05-09 00:09:01 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  // add this so Vite knows your repo’s sub-path on GH-Pages
+  base: '/A2vissionweb/',
+  build: {
+    outDir: 'docs',     // emit to `docs/` so GH-Pages can serve it
+    emptyOutDir: true,
+  },
   plugins: [react()],
-})
+});
