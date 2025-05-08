429d5576465f3e6065409d0998ce432e953cc71d
Aaron200497 <98113453+Aaron200497@users.noreply.github.com>
2025-05-08 21:07:20 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  base: '/A2vissionweb/',    // el nombre de tu repo
+  plugins: [react()]
 })
