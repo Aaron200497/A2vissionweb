8666db3802f345617a9b5bf3a06d1c410fe2b82a
Aaron200497 <98113453+Aaron200497@users.noreply.github.com>
2025-05-08 21:39:43 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/A2vissionweb/',    // el nombre de tu repo
-  plugins: [react()]
+  plugins: [react()],
 })
