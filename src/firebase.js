c4da89870a55455075de04a93c9e1b124d18b8d0
Aaron200497 <98113453+Aaron200497@users.noreply.github.com>
2025-05-14 13:35:06 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -20,7 +20,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyACuVKmVc7xS4K5TVljAeQTq7WNP_UC8n0",
   authDomain: "a2vissionweb.firebaseapp.com",
   projectId: "a2vissionweb",
-  storageBucket: "a2vissionweb.firebasestorage.app",
+  storageBucket: "a2vissionweb.appspot.com",
   messagingSenderId: "127311201904",
   appId: "1:127311201904:web:1b3bc95feb069bdc7d56b5",
   measurementId: "G-25MGLNYKEL"
